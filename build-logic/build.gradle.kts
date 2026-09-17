70f01082aa7e72dc27ebba1cd6308bb1f5d7a32a
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-09-17 00:51:25 -0500

    Fix Android platform CI plugin resolution

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
 dependencies {
     implementation("com.android.tools.build:gradle:8.10.1")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.21")
+    implementation("org.jetbrains.kotlin:compose-compiler-gradle-plugin:2.1.21")
 }
 
 kotlin {
